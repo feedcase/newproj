0e1bc984d102e3042581d1d11cf451e1bc0ccf46
feedcase <seva1502@gmail.com>
2020-05-06 19:24:32 +0300

    Completed task

--- task1.1.py
+++ task1.1.py
@@ -1,7 +1,7 @@
 N = input('Input ur string: ')
 l = len(N)//2
 for letter in range(l):
-    if N[letter] != N[-1]:
+    if N[letter] != N[-1 - letter]:
         print('Not palindrome')
         quit()
 print('Its palindrome')
