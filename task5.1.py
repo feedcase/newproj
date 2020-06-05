29a02106f5a83a9fa92ce253e5d7a479b109cf43
feedcase <seva1502@gmail.com>
2020-06-05 19:55:29 +0300

    hw5

---
+++ task5.1.py
@@ -0,0 +1,23 @@
+class readonly(object):
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, owner):
+        return self.func(instance)
+
+    def __set__(self, instance, value):
+        raise AttributeError()
+
+
+class Test(object):
+    def __init__(self):
+        self.__val = 1
+
+    @readonly
+    def val(self):
+        return self.__val
+
+
+test = Test()
+print(test.val)  # OK, выводит 1
+test.val = 2  # AttributeError
